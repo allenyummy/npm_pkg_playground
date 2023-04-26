789f2e5622c903ff9f971f2e9456b3da72f34334
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2023-04-26 22:53:37 +0800

    test: new workfldafad

--- src/src.ts
+++ src/src.ts
@@ -1,5 +1,5 @@
 function main() {
   console.log("normal");
   console.log("hdhgfh");
-  console.log("test new workflowddabadggfdsafcddddddd");
+  console.log("test new wordkflowddabadggfdsafcddddddd");
 }
